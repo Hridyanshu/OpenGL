7097a85f609c3d032b84dd86317cf63aa2dc5dfd
Hridyanshu <hridyanshusharma@gmail.com>
2020-09-01 13:00:01 +0530

    Horizontal stripes on half part of the triangle

--- triangleStripes.cpp
+++ triangleStripes.cpp
@@ -28,6 +28,52 @@ void display()
 		glVertex2f(0.0,0.5);
 	glEnd();
 		
+	//stripes horizontal
+	glBegin(GL_LINES);
+		glVertex2f(0.0,-0.4);
+		glVertex2f(-0.45,-0.4);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glVertex2f(0.0,-0.3);
+		glVertex2f(-0.4,-0.3);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glVertex2f(0.0,-0.2);
+		glVertex2f(-0.35,-0.2);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glVertex2f(0.0,-0.1);
+		glVertex2f(-0.3,-0.1);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glVertex2f(0.0,0.0);
+		glVertex2f(-0.25,0.0);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glVertex2f(0.0,0.1);
+		glVertex2f(-0.2,0.1);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glVertex2f(0.0,0.2);
+		glVertex2f(-0.15,0.2);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glVertex2f(0.0,0.3);
+		glVertex2f(-0.1,0.3);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glVertex2f(0.0,0.4);
+		glVertex2f(-0.05,0.4);
+	glEnd();
+	
 	glFlush();
 }
 
