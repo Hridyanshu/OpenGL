0d18fe9c59fde5c845912672d3c9081b4f813d9b
Hridyanshu <hridyanshusharma@gmail.com>
2020-11-03 12:02:21 +0530

    Added a function to make car

--- Motion/car.cpp
+++ Motion/car.cpp
@@ -12,3 +12,33 @@ void init()
 	
 	gluOrtho2D(-100, 100, -100, 100);
 }
+
+
+void car(int z) //z is the no of time the function is called
+{
+	float x,y;
+	glBegin(GL_POINTS);
+		for(float i=0;i<(2*pi);i=i+0.001)
+		{
+			x = 5 * cos(i);
+			y = 5 * sin(i);
+			glVertex2f(x-50+25*(z-1),y);
+		}
+		for(float i=0;i<(2*pi);i=i+0.001)
+		{
+			x = 5 * cos(i);
+			y = 5 * sin(i);
+			glVertex2f(x-25+25*(z-1),y);
+		}
+	glEnd();
+	glBegin(GL_LINES);
+		glVertex2f(-50+25*(z-1),5);
+		glVertex2f(-50+25*(z-1),10);
+		glVertex2f(-50+25*(z-1),5);
+		glVertex2f(-25+25*(z-1),5);
+		glVertex2f(-25+25*(z-1),5);
+		glVertex2f(-25+25*(z-1),10);
+		glVertex2f(-50+25*(z-1),10);
+		glVertex2f(-25+25*(z-1),10);
+	glEnd();
+}
