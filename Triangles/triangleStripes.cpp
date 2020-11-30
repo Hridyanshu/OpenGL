9c98166127672c57bb9e1b06892de0d80dac8814
Hridyanshu <hridyanshusharma@gmail.com>
2020-11-30 14:38:11 +0530

    A triangle with stripes

---
+++ Triangles/triangleStripes.cpp
@@ -0,0 +1,88 @@
+//a triangle with stripes inside it
+
+#include<GL/gl.h>
+#include<GL/glu.h>
+#include<GL/glut.h>
+#include<math.h> 
+#define pi 3.142857
+
+void CreateLine(float x1, float y1, float x2, float y2)
+{
+	glBegin(GL_LINES);
+	glVertex2f(x1,y1);
+	glVertex2f(x2,y2);
+	glEnd();
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);	
+	
+	glBegin(GL_POINTS); 
+	float x, y, i;	
+ 	for ( i = 0; i < (2 * pi); i += 0.001) 
+	{ 
+		
+		x = 0.2 * cos(i); 
+		y = 0.2 * sin(i); 
+		
+		glVertex2i(x, y); 
+	} 
+	glEnd();
+
+	
+	CreateLine(-0.5,-0.5,0.5,-0.5);
+	CreateLine(-0.5,-0.5,0.0,0.5);
+	CreateLine(0.5,-0.5,0.0,0.5);
+	CreateLine(0.0,-0.5,0.0,0.5);
+		
+	//stripes horizontal
+	CreateLine(0.0,-0.4,-0.45,-0.4);
+	CreateLine(0.0,-0.3,-0.4,-0.3);
+	CreateLine(0.0,-0.2,-0.35,-0.2);
+	CreateLine(0.0,-0.1,-0.3,-0.1);
+	CreateLine(0.0,0.0,-0.25,0.0);
+	CreateLine(0.0,0.1,-0.2,0.1);
+	CreateLine(0.0,0.2,-0.15,0.2);
+	CreateLine(0.0,0.3,-0.1,0.3);
+	CreateLine(0.0,0.4,-0.05,0.4);
+	
+	//stripes diagonal
+	CreateLine(0.45,-0.5,0.475,-0.45);
+	CreateLine(0.4,-0.5,0.45,-0.4);
+	CreateLine(0.35,-0.5,0.425,-0.35);
+	CreateLine(0.3,-0.5,0.4,-0.3);
+	CreateLine(0.25,-0.5,0.375,-0.25);
+	CreateLine(0.2,-0.5,0.35,-0.2);
+	CreateLine(0.15,-0.5,0.325,-0.15);
+	CreateLine(0.1,-0.5,0.3,-0.1);
+	CreateLine(0.05,-0.5,0.275,-0.05);
+	CreateLine(0.0,-0.5,0.25,0.0);
+	CreateLine(0.0,-0.4,0.225,0.05);
+	CreateLine(0.0,-0.3,0.2,0.1);
+	CreateLine(0.0,-0.2,0.175,0.15);
+	CreateLine(0.0,-0.1,0.15,0.2);
+	CreateLine(0.0,0.0,0.125,0.25);
+	CreateLine(0.0,0.1,0.1,0.3);
+	CreateLine(0.0,0.2,0.075,0.35);
+	CreateLine(0.0,0.3,0.05,0.4);
+	CreateLine(0.0,0.4,0.025,0.45);
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	
+	glutInitDisplayMode(GLUT_RGB);
+	
+	glutInitWindowPosition(100,200);
+	glutInitWindowSize(1500,1500);
+	glutCreateWindow("Triangle with Stripes");
+
+	glutDisplayFunc(display);
+	
+	glutMainLoop();
+	
+	return 0;
+}
